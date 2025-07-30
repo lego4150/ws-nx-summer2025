76c26c019f44fba398ee6473e019c0e7d499dc5e
nvarma <nvarma@ivycomptech.com>
2025-07-30 23:39:57 +0530

    text nx pipeline

--- apps/movies/src/app/utils/search-movies-by-title.util.ts
+++ apps/movies/src/app/utils/search-movies-by-title.util.ts
@@ -2,12 +2,12 @@ import { Movie } from '@react-monorepo/shared-models';
 
 export const searchMoviesByTitle = (
   movies: Movie[],
-  query?: string | null
+  query?: string | null,
+  test: string = ''
 ): Movie[] => {
   if (!query || query?.length === 0) {
     return movies;
   }
-
   return movies.filter((movie) =>
     movie.title.toLowerCase().includes(query.toLowerCase())
   );
